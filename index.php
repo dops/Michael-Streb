f457b2cf0c98acbc8cbcce6ae1a553d4bcf0e110
Michael Streb <michael.streb@michael-streb.de>
2011-03-04 16:29:05 +0100

    Hotfix

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ define('BASE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/');
 
 echo '<?xml version="1.0" encoding="utf-8"?>';
 
+// Comment
+
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
